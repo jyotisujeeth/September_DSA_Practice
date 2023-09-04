751f76c2c9b44e8db5aa30f004bf0acf3594f93d
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-04 15:52:21 +0530

    Create LinkedList.js

---
+++ LinkedList.js
@@ -0,0 +1,14 @@
+/*
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+*/
+var hasCycle = (head) => {
+  let fast = head;
+  while (fast && fast.next) {
+    head = head.next;
+    fast = fast.next.next;
+    if (head === fast) return true;
+  }
+  return false;
+};
