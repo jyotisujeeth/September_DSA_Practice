ec2444595a51a3218d5fc544b179c4f4c18099f6
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-04 15:53:13 +0530

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -12,3 +12,37 @@ var hasCycle = (head) => {
   }
   return false;
 };
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+Using Hash Map
+var hasCycle = function(head) {
+    const seen = new Set();
+    
+    function traverse(node) {
+        if(seen.has(node)) return true;
+        if(!node) return false;
+        seen.add(node);
+        return traverse(node.next);
+    }
+    return traverse(head);
+};
+Solution-2: Two Pointers
+var hasCycle = function(head) {
+    
+    function run(fast, slow) {
+        if(!fast || !fast.next) return false;
+        if(fast.next === slow) return true;
+        return run(fast.next.next, slow.next);
+    }
+    return run(head, head);    
+};
+Solution-3: Add Property
+var hasCycle = function(head) {
+    
+    function run(node) {
+        if(!node) return false;
+        if(node.seen) return true;
+        node.seen = true;
+        return run(node.next);
+    }
+    return run(head);
+};
